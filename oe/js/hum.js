3c57e3e38e219ef1e624f16f85ce88f3d4818f75
Danylo Kolesnikov <danylokolesnikov@gmail.com>
2017-01-20 03:09:04 +0200

    final update nav

--- oe/js/hum.js
+++ oe/js/hum.js
@@ -1,25 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
   var button = document.querySelector('.hamburger--collapse');
   var menuItems = document.querySelectorAll('.nav_item');
+  var links = document.querySelectorAll('.nav_item a');
+  links = Array.prototype.slice.apply(links);
   menuItems = Array.prototype.slice.apply(menuItems);
 
   button.addEventListener('click', function() {
-    console.log(menuItems)
     if (button.className === "hamburger hamburger--collapse") {
-      button.classList.add('is-active');
       showMenu();
     }
     else {
-      button.className = 'hamburger hamburger--collapse';
       hideMenu();
     };
   })
+
+  links.forEach(function (el){
+    el.addEventListener('click', function() {
+      hideMenu();
+    })
+
+  })
 function showMenu() {
+  button.classList.add('is-active');
   menuItems.map(function (el){
     el.style.display = 'list-item';
   })
 };
 function hideMenu() {
+  button.className = 'hamburger hamburger--collapse';
   menuItems.map(function (el){
     el.style.display = 'none';
   });
